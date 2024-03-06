b639c368d361ddfc6c1a54a1e95dc249042bff67
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-06 15:23:47 -0400

    feat: use get contries

---
+++ app/interfaces/country.ts
@@ -0,0 +1,12 @@
+export interface countryData {
+  cca2: string;
+  flags: {
+    svg: string;
+  };
+  name: {
+    common: string;
+  };
+  population: number;
+  region: string;
+  capital: string;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import Card from "./components/cards/cards";
+import { countryData } from "./interfaces/country";
+import { getCountries } from "./actions/data";
 
 export default function Home() {
+  const [countries, setCountries] = useState<countryData[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data: countryData[] = await getCountries();
+        setCountries(data);
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <main className="bg-veryLightGray_LightModeBG dark:bg-veryDarkBlue_DarkModeBG ">
       <section className="grid grid-cols-1 mobile:grid-cols-2 desktop:grid-cols-4 mobile:gap-10 gap-20 py-4 px-2 mobile:px-20">
-        {Array.from({ length: 8 }).map((_, index) => (
+        {countries.map((country, index) => (
           <Card
             key={index}
-            flagSrc="https://restcountries.com/data/afg.svg"
-            countryName="Afghanistan"
-            population={27657145}
-            region="Asia"
-            capital="Kabul"
+            flagSrc={country.flags.svg}
+            countryName={country.name.common}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
           />
         ))}
       </section>
