eb6631a7906fbe86536f7e8c4077c72ffda9d35e
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-06 15:08:49 -0400

    feat: create card components

---
+++ app/components/cards/cards.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+interface CountryCardProps {
+  flagSrc: string;
+  countryName: string;
+  population: number;
+  region: string;
+  capital: string;
+}
+
+const Card = (countryProps: CountryCardProps) => {
+  return (
+    <div className="flex flex-col bg-white_DarkModeText_LightModeElements dark:bg-darkBlue_DarkModeElements dark:text-white_DarkModeText_LightModeElements mobile:max-w-80 desktop:w-80 rounded-lg shadow-sm overflow-hidden">
+      <div className="">
+        <Image
+          src={countryProps.flagSrc}
+          alt="country flag"
+          width={0}
+          height={0}
+          style={{
+            width: "320px",
+            height: "160px",
+            objectFit: "fill",
+          }}></Image>
+      </div>
+      <div className="py-8 px-6">
+        <p className="font-bold text-2xl mb-6">{countryProps.countryName}</p>
+        <p className="font-semibold pb-2 text-lg">
+          Population:{" "}
+          <span className="font-normal">
+            {countryProps.population.toLocaleString("en-US")}
+          </span>
+        </p>
+        <p className="font-semibold pb-2 text-lg">
+          Region: <span className="font-normal">{countryProps.region}</span>
+        </p>
+        <p className="font-semibold text-lg">
+          Capital: <span className="font-normal">{countryProps.capital}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import Card from "./components/cards/cards";
+
 export default function Home() {
   return (
-    <main className="bg-veryLightGray_LightModeBG dark:bg-veryDarkBlue_DarkModeBG ">Hello World</main>
+    <main className="bg-veryLightGray_LightModeBG dark:bg-veryDarkBlue_DarkModeBG ">
+      {
+        Array.from({ length: 6 }).map((_, index) => (
+          <Card
+            key={index}
+            flagSrc="https://restcountries.com/data/afg.svg"
+            countryName="Afghanistan"
+            population={27657145}
+            region="Asia"
+            capital="Kabul"
+          />
+        ))
+      }
+    </main>
   );
 }
