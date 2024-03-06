1565d687ae3c44126ebd1abf446fbc7d8bf1e8f5
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-06 14:51:57 -0400

    feat: create switcher for change the theme and header for use that

---
+++ app/components/header/header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import ThemeSwitcher from "../theme/theme_switcher";
+
+const Header = () => {
+  const router = useRouter();
+  return (
+    <>
+      <header className="sticky top-0 z-10 flex flex-row justify-between items-centre py-6 px-20 bg-white_DarkModeText_LightModeElements dark:bg-darkBlue_DarkModeElements dark:text-white_DarkModeText_LightModeElements shadow-[rgba(133,133,133,0.20)_0px_2px_4px_1px]">
+        <button
+          onClick={() => {
+            router.push("/");
+          }}>
+          <h1 className="font-extrabold text-3xl">Where in the world?</h1>
+        </button>
+        <ThemeSwitcher />
+      </header>
+    </>
+  );
+};
+
+export default Header;
