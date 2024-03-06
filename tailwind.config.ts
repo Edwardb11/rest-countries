84ef10c670e9bbaf7ff68ddac1302aa8def9f1fd
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-06 14:51:17 -0400

    feat: configure the color for the challenge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,23 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
+    colors: {
+      darkBlue_DarkModeElements: "hsl(209, 23%, 22%)",
+      veryDarkBlue_DarkModeBG: "hsl(207, 26%, 17%)",
+      veryDarkBlue_LightModeText: "hsl(200, 15%, 8%)",
+      darkGray_LightModeInput: "hsl(0, 0%, 52%)",
+      veryLightGray_LightModeBG: "hsl(0, 0%, 98%)",
+      white_DarkModeText_LightModeElements: "hsl(0, 0%, 100%)",
+    },
+    screens: {
+      mobile: "376px",
+      desktop: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        full: "0 0 4px 2px rgba(0, 0, 0, 0.08)",
       },
     },
   },
