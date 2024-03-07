37865ae9bdc7eaf8e29ec5f63494d87239c4ec98
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-06 21:18:37 -0400

    feat: add component in  slug for the request loading or error

---
+++ app/details/[slug]/error.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex h-screen flex-col items-center justify-center dark:bg-veryDarkBlue_DarkModeBG">
+      <h2 className="text-center dark:text-white_DarkModeText_LightModeElements">
+        Something went wrong!
+      </h2>
+      <button
+        className="mt-4 rounded-md bg-darkGray_LightModeInput dark:bg-darkBlue_DarkModeElements px-4 py-2 text-sm text-veryDarkBlue_LightModeText dark:text-white_DarkModeText_LightModeElements hover:bg-veryDarkBlue_LightModeText hover:text-veryLightGray_LightModeBG"
+        onClick={() => reset()}>
+        Try again
+      </button>
+    </main>
+  );
+}
