e245839c996fc82c09e52787afd9daa464cfda22
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-06 21:07:46 -0400

    feat: more query for get contry

--- app/actions/data.ts
+++ app/actions/data.ts
@@ -7,3 +7,33 @@ export const getCountries = async () => {
 
   return countryData.json();
 };
+
+export const getCountryByCode = async (code: string) => {
+  const country = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+
+  if (!country.ok) {
+    throw new Error("Failed to fetch detailed country data");
+  }
+
+  return country.json();
+};
+
+export const getCountryNameByCode = async (codes: string[]) => {
+  if (!codes) {
+    return null;
+  }
+  const names = await Promise.all(
+    codes.map(async (code) => {
+      const countryName = await fetch(
+        `https://restcountries.com/v3.1/alpha/${code}?fields=name,cca2`
+      );
+
+      if (!countryName.ok) {
+        throw new Error("Failed to fetch detailed country data");
+      }
+
+      return countryName.json();
+    })
+  );
+  return names;
+};
